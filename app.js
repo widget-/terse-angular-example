6705ff37fd1710e1606daf7a652e8f8b572f53bf
Bryan <bryan@align.us>
2015-10-12 19:25:07 -0500

    small fixes

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 var app = angular.module('myApp', ['myExternalModule']);
 
 app.controller('myController', function ($scope, myService, myFactory, myConstant) {
-	var vm = this;
-	vm.in = "Hello, world!"
-	
-	$scope.$watch('vm.in', function(newVal, oldVal) {
-		vm.out = myFactory.repeat(newVal) + myConstant;
-		vm.count = myService.inc();
-	})
+  var vm = this;
+  vm.in = "Hello, world!"
+  
+  $scope.$watch('vm.in', function(newVal, oldVal) {
+    vm.out = myFactory.repeat(newVal) + myConstant;
+    vm.count = myService.inc();
+  })
 });
 
-app.service('myService', function(myValue) {
-	this.inc = function() {
-		myValue += 1;
-		return myValue;
-	}
+app.service('myService', function() {
+  var val = 0;
+  this.inc = function() {
+    val += 1;
+    return val;
+  }
 });
 
 app.factory('myFactory', function() {
-	var factory = {};
-	factory.repeat = function(s) {
-		return s + " " + s;
-	}
-	return factory;
+  var factory = {};
+  factory.repeat = function(s) {
+    return s + " " + s;
+  }
+  return factory;
 })
 
 app.constant('myConstant', '!');
@@ -30,34 +31,29 @@ app.constant('myConstant', '!');
 app.value('myValue', 0);
 
 app.directive('myElementDirective', function() {
-	return {
-		restrict: 'E',
-		transclude: true,
-		template: '<div style="border: 1px solid red;" ng-transclude></div>'
-	}
+  return {
+    restrict: 'E',
+    transclude: true,
+    template: '<div style="border: 1px solid red;" ng-transclude></div>'
+  }
 });
 
 var externalModule = angular.module('myExternalModule', []);
 
 externalModule.directive('myAttributeDirective', function() {
-	return {
-		restrict: 'A',
-		link: function(scope, element, attrs) {
-			var hue = 0;
-			var changeColor = function() {
-				hue = (hue + 5) % 360;
-				element.css('color', 'hsl('+hue+',100%,30%)')
-				setTimeout(changeColor, 100);
-			}
-			changeColor();
-		}
-	}
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      console.log(attrs)
+      element.css('color', attrs.myAttributeDirective || 'red');
+    }
+  }
 });
 
 app.filter('myFilter', function() {
-	return function(input, option) {
-		return input.split('').sort(function(a,b) {
-			return (option) ? (a<b) : (a>b);
-		}).join('');
-	}
+  return function(input, option) {
+    return input.split('').sort(function(a,b) {
+      return (option) ? (a<b) : (a>b);
+    }).join('');
+  }
 });
